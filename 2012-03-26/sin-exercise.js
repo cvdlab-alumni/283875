c7ba237dbc71b8b85040213d9f4998b899c5a771
Marco Liceti <marco.liceti@gmail.com>
2012-03-26 17:01:07 +0200

    sin exercise fixed

--- 2012-03-26/sin-exercise.js
+++ 2012-03-26/sin-exercise.js
@@ -2,7 +2,7 @@ var domain = DOMAIN([[0,2*Math.PI]])([10]);
 var mapping = function (p) {
   var u = p[0];
 
-  return [u,Math.sin(u)];
+  return [u,SIN(u)];
 };
 var mapped = MAP(mapping)(domain);
 DRAW(mapped);
