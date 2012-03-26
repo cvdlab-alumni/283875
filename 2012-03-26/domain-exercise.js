d624b18f2f8d7a1223d15984283838eb2fce01a6
Marco Liceti <marco.liceti@gmail.com>
2012-03-26 16:50:49 +0200

    DOMAIN exercise

---
+++ 2012-03-26/domain-exercise.js
@@ -0,0 +1,6 @@
+var domain = DOMAIN([[1.5,5.5]])([4]);
+DRAW(domain);
+var domain2 = DOMAIN([[1.5,5.5],[1,3]])([4,2]);
+DRAW(domain2);
+var domain3 = DOMAIN([[1.5,5.5],[1,3],[0,1]])([4,2,1]);
+DRAW(domain3);
